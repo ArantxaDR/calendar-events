304507b1c90f3b7dbea9598bde752c56f42e3d59
ArantxaDR <arantxaderuiz@gmail.com>
2022-11-10 14:27:35 +0100

    add fullcalendar to dependencies

--- front-calendar-events/src/App.tsx
+++ front-calendar-events/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>estamos probando</h1>
       </header>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth" />
     </div>
   );
 }
