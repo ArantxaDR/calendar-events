c22d2d65456f6825eb821ca7be4e60ced9a141cd
ArantxaDR <arantxaderuiz@gmail.com>
2022-11-10 16:08:42 +0100

    fix cors error

--- back-calendar-events/index.js
+++ back-calendar-events/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3001;
 const calendarEventsRouter = require("./routes/calendarEvents");
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(
 	express.urlencoded({
