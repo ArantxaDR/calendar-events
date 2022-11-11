4ab5524bba9853d0828d58543131b5a67f43ff69
ArantxaDR <arantxaderuiz@gmail.com>
2022-11-12 00:49:21 +0100

    add toggle to change language

--- front-calendar-events/src/components/header/Header.tsx
+++ front-calendar-events/src/components/header/Header.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import './Header.scss';
+import Switch from '@mui/material/Switch';
 import { useTranslation } from 'react-i18next';
+import { Stack, Typography } from '@mui/material';
+import './Header.scss';
+
 
 function Header() {
 	const [t, i18n] = useTranslation("global");
+	const [checked, setChecked] = React.useState(false);
+
 
 	const handdlerChangeLanguage = (ev: any) => {
-		i18n.changeLanguage(ev.target.value);
+		let language: string = (checked === true) ? "en" : "es";
+		i18n.changeLanguage(language);
+		setChecked(!checked);
 	}
 
 	return (
 		<header className='header'>
 			<h1>{t("header.title")}</h1>
-			<select onChange={handdlerChangeLanguage}>
-				<option value="en">English</option>
-				<option value="es">Español</option>
-			</select>
+			<Stack className='toggle' direction="row" spacing={1} alignItems="center">
+				<Typography>{t("header.english")}</Typography>
+				<Switch
+					checked={checked}
+					onChange={handdlerChangeLanguage}
+					inputProps={{ 'aria-label': 'controlled' }} />
+				<Typography>{t("header.spanish")}</Typography>
+			</Stack>
 		</header>
 	)
 }
