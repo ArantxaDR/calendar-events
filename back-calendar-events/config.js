24dca89adac816b6e5e297cdbe6716784726a8ed
ArantxaDR <arantxaderuiz@gmail.com>
2022-11-11 23:15:54 +0100

    delete time zone config

--- back-calendar-events/config.js
+++ back-calendar-events/config.js
@@ -3,10 +3,8 @@ const config = {
 		host: "sql7.freemysqlhosting.net",
 		user: "sql7557614",
 		password: "dQhFHqUShc",
-		database: "sql7557614",
-		timezone: '+00:00'
-	},
-	listPerPage: 10,
+		database: "sql7557614"
+	}
 };
 
 module.exports = config;
